d54fd3ce3583344f1fae0b77bed3ec303b0b2362
Aidan Fitzpatrick <aidan@reincubate.com>
2018-02-10 13:46:49 +0000

    Bump version. HTTP -> HTTPS. Fixed mimetype bug.

--- trackback/utils/resolvers.py
+++ trackback/utils/resolvers.py
@@ -5,32 +5,32 @@ def decorated(target_url):
     """
     Returns the pinged object, if the view was decorated with the
     ``receives_pingbacks_for`` decorator.
-    
-    """   
+
+    """
     try:
         urlresolver = get_resolver(None)
         site = Site.objects.get_current()
-        func, args, kwargs = urlresolver.resolve(target_url.replace("http://%s"%site.domain, ''))
-        
+        func, args, kwargs = urlresolver.resolve(target_url.replace("https://%s"%site.domain, ''))
+
         if hasattr(func, 'pingback_object'):
             obj = func.pingback_object
             return obj
-        return None    
+        return None
     except (NoReverseMatch, Resolver404), e:
-        return None        
-            
+        return None
+
 
 def generic_view(target_url):
     """
     Tries to figure out, of the view is a generic view and determines which
     object is served by the view.
-    
+
     """
     try:
         urlresolver = get_resolver(None)
         site = Site.objects.get_current()
-        func, args, kwargs = urlresolver.resolve(target_url.replace("http://%s"%site.domain, ''))
-        
+        func, args, kwargs = urlresolver.resolve(target_url.replace("https://%s"%site.domain, ''))
+
         if func.__name__ == 'object_detail':
             # may be django's generic view or something which at least works in an similar fashion
             if 'object_id' in kwargs:
@@ -46,15 +46,14 @@ def generic_view(target_url):
                         return obj
                     except Exception, e:
                         pass
-                        
+
             elif 'slug' in kwargs and 'slug_field' in kwargs:
                 try:
                     obj = kwargs['queryset'].get(**{kwargs['slug_field']: kwargs['slug'],})
                     return obj
                 except Exception, e:
                     pass
-                    
-        return None    
+
+        return None
     except (NoReverseMatch, Resolver404), e:
         return None
-            
--- trackback/views.py
+++ trackback/views.py
@@ -15,12 +15,12 @@ def receive_trackback(request, content_type_id, object_id, form_class, template_
     """
     Endpoint to receive a trackback for any of our objects, described by
     ``content_type_id`` together with ``object_id``.
-    
+
     """
     ctype = get_object_or_404(ContentType, pk=content_type_id)
     obj = get_object_or_404(ctype.model_class(), pk=object_id)
     site = Site.objects.get_current()
-    
+
     form = form_class(request.POST)
     if form.is_valid():
         trackback = form.save(commit=False)
@@ -34,8 +34,8 @@ def receive_trackback(request, content_type_id, object_id, form_class, template_
                    'message': "\n".join(form.errors)
                   }
         return render_to_response(template_name, context, mimetype="text/xml")
-    
-    
+
+
 
 @csrf_exempt
 def receive_pingback(request):
@@ -43,23 +43,19 @@ def receive_pingback(request):
     Handles XML-RPC Pingbacks.
     Spec: http://www.hixie.ch/specs/pingback/pingback
     Also see: http://code.djangoproject.com/wiki/XML-RPC
-    
+
     """
     if request.method == 'POST' and len(request.POST):
         try:
             response = HttpResponse(
                             xmlrpc_dispatcher._marshaled_dispatch(
-                                        request.raw_post_data, 
+                                        request.raw_post_data,
                                         dispatch_method=curry(xmlrpc_dispatcher._dispatch, request=request)
                             )
                         )
         except Exception, e:
             #print e
             raise e #FIXME: handle errors gracefully if possible
-        
+
         return response
-    return HttpResponse(xmlrpc_dispatcher.system_listMethods(), content_type="'text/plain; charset=utf-8")
-    
-    
-   
-   
+    return HttpResponse(xmlrpc_dispatcher.system_listMethods(), content_type="text/plain; charset=utf-8")
